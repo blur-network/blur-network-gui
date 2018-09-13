a2b7b38f2ba6a5789a2b4341f79fa73a6d905d7e
The Blur Network <admin@blur.cash>
2018-09-13 08:10:13 -0400

    Unhide starter shell script

---
+++ build/release/bin/start_gui_wallet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./blurd --seed-node 178.128.191.245:14894 --seed-node 178.128.186.101:14894 --seed-node 178.128.180.136:14894 --hide-my-port --no-igd --p2p-bind-port 14894 --rpc-bind-ip 127.0.0.1 --rpc-bind-port 14895 --detach --non-interactive && ./blur-gui-wallet
