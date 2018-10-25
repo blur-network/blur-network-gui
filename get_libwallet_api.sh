e9a4e1b9541e73952269a42dbcc6f2d014985ef0
b <b@bs-MacBook-Pro.local>
2018-10-25 07:29:30 -0400

    Update monero submodule

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MONERO_URL=https://github.com/blur-network/blur.git
-MONERO_BRANCH=v0.1.7
+MONERO_BRANCH=v0.1.7.5
 
 pushd $(pwd)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout -B v0.1.7
+git -C $MONERO_DIR checkout -B v0.1.7.5
 
 
 
