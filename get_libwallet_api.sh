655408fa6d10a4f20c21ea143d2ad50809c93b6c
who-biz <37732338+who-biz@users.noreply.github.com>
2019-03-03 06:05:36 -0500

    Change branch for submodule build

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MONERO_URL=https://github.com/blur-network/blur.git
-MONERO_BRANCH=stable
+MONERO_BRANCH=v0.1.9.1
 
 pushd $(pwd)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout stable
+git -C $MONERO_DIR checkout v0.1.9.1
 
 
 
