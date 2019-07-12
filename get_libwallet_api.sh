3a55ad2311476acf31a987b6b0eee266704fb75d
who-biz <37732338+who-biz@users.noreply.github.com>
2019-07-12 04:17:09 -0400

    ditto as previous commit

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MONERO_URL=https://github.com/blur-network/blur.git
-MONERO_BRANCH=v0.1.9.7
+MONERO_BRANCH=master
 
 pushd $(pwd)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout v0.1.9.7
+git -C $MONERO_DIR checkout master
 
 
 
